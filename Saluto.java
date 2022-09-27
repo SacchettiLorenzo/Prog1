8bcca69f968222ea95fb84a38dffda654e2cc785
SacchettiLorenzo <sacchettibusiness@gmail.com>
2022-09-27 15:27:48 +0200

    prima lezione, input e output testuale

--- Saluto.java
+++ Saluto.java
@@ -1,6 +1,16 @@
-class Saluto{
+import java.util.Scanner;
+// una classe non pubblica può essere descritta in un file con nome diverso dal nome dela classe
+public class Saluto{
     public static void main(String[] args){
         System.out.println("Ciao");
         System.out.println("Prova");
+        
+        Scanner scanner = new Scanner(System.in);
+
+        String line = scanner.nextLine();
+
+        scanner.close();
+
+        System.out.println(line);
     }
 }
