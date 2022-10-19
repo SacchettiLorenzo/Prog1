561f1d5333b0774a5c964b6d34f4b9f69b1fd604
SacchettiLorenzo <sacchettibusiness@gmail.com>
2022-10-20 01:02:38 +0200

    nuovi esercizi

---
+++ Numbers.java
@@ -0,0 +1,13 @@
+public class Numbers {
+    public static void main(String[] args) {
+        System.out.println(0b10010010);//binary
+        System.out.println(0x22);//hex
+        System.out.println(15e-3f);//exp float
+        System.out.println(2.0e10);//exp double
+        System.out.println(1000000000000000000l);//long
+        System.out.println(0127);//octal
+        System.out.println();//
+        System.out.println();//
+
+    }
+}
