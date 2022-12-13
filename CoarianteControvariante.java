2441f16ac61e31282f8beab3424dbe9459fe4d46
SacchettiLorenzo <sacchettibusiness@gmail.com>
2022-12-13 11:52:31 +0100

    stampa ricorsiva variante covariante dicotomica

--- CoarianteControvariante.java
+++ CoarianteControvariante.java
@@ -1,22 +1,63 @@
 public class CoarianteControvariante {
     public static void main(String[] args) {
         int[] dataset = {10,20,30,50,60,4,3,3,576,12,678};
-        stampaRicCon(dataset, 0);
+        stampaArrayRicCon(dataset);
+        stampaArrayRicCov(dataset);
+        stampaArrayRicDicotomic(dataset);
     }
 
-    // stampa controvariante
+    //NOTE -  stampa controvariante
     // l'indice è inizializzato a 0;
-    static void stampaRicCon(int[] x, int i){
+
+    //NOTE -  stampa covariante
+    // l'indice è inizializzato alla lunghezza dell'array
+
+    //NOTE - stampa dicotomica
+    //ogni volta viene diviso in 2 il numero di elemanti considerati
+    //ha 3 parametri formali (vettore, indici di sinistra, indici di destra)
+
+    static void stampaArrayRicCon(int[] x){
+        stampaArrayRicCon(x,0);
+    }
+
+    static void stampaArrayRicCon(int[] x, int i){
         if(i<x.length){
             System.out.println(x[i] + " ");
-            stampaRicCon(x, i+1);
+            stampaArrayRicCon(x, i+1);
         }else{
             System.out.println();
         }
     }
 
     //TODO: implementare gli altri metodi (covariante, dicotomico)
-    // il metodo dicotomico ha 3 parametri formali (vettore, indici di sinistra, indici di destra)
+
+    static void stampaArrayRicCov(int[] x){
+        stampaArrayRicCov(x, x.length-1);
+    }
+
+    static void stampaArrayRicCov(int[] x, int i){
+        if(i >= 0){
+            System.out.println(x[x.length-1-i] + "");
+            stampaArrayRicCov(x, i-1);
+        }else{
+            System.out.println();
+        }
+    }
+
+    static void stampaArrayRicDicotomic(int x[]){
+        stampaArrayRicDicotomic(x, 0, x.length-1);
+    }
+
+    static void stampaArrayRicDicotomic(int x[], int left, int right){
+        int mid = (left+right)/2;
+
+        if(left != right){
+            stampaArrayRicDicotomic(x, left, mid);
+            stampaArrayRicDicotomic(x, mid+1, right);
+        }else{
+            System.out.println(x[left]);
+        }
+    }
 
     
 }
