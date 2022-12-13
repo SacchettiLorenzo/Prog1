fa8ddb9be00770445c450bffbc42ec8f615da411
SacchettiLorenzo <sacchettibusiness@gmail.com>
2022-12-13 12:13:14 +0100

    lazy and bitwise

--- CoarianteControvariante.java
+++ CoarianteControvariante.java
@@ -1,63 +1,62 @@
 public class CoarianteControvariante {
     public static void main(String[] args) {
-        int[] dataset = {10,20,30,50,60,4,3,3,576,12,678};
+        int[] dataset = { 10, 20, 30, 50, 60, 4, 3, 3, 576, 12, 678 };
         stampaArrayRicCon(dataset);
         stampaArrayRicCov(dataset);
         stampaArrayRicDicotomic(dataset);
     }
 
-    //NOTE -  stampa controvariante
+    // NOTE - stampa controvariante
     // l'indice è inizializzato a 0;
 
-    //NOTE -  stampa covariante
+    // NOTE - stampa covariante
     // l'indice è inizializzato alla lunghezza dell'array
 
-    //NOTE - stampa dicotomica
-    //ogni volta viene diviso in 2 il numero di elemanti considerati
-    //ha 3 parametri formali (vettore, indici di sinistra, indici di destra)
+    // NOTE - stampa dicotomica
+    // ogni volta viene diviso in 2 il numero di elemanti considerati
+    // ha 3 parametri formali (vettore, indici di sinistra, indici di destra)
 
-    static void stampaArrayRicCon(int[] x){
-        stampaArrayRicCon(x,0);
+    static void stampaArrayRicCon(int[] x) {
+        stampaArrayRicCon(x, 0);
     }
 
-    static void stampaArrayRicCon(int[] x, int i){
-        if(i<x.length){
+    static void stampaArrayRicCon(int[] x, int i) {
+        if (i < x.length) {
             System.out.println(x[i] + " ");
-            stampaArrayRicCon(x, i+1);
-        }else{
+            stampaArrayRicCon(x, i + 1);
+        } else {
             System.out.println();
         }
     }
 
-    //TODO: implementare gli altri metodi (covariante, dicotomico)
+    // TODO: implementare gli altri metodi (covariante, dicotomico)
 
-    static void stampaArrayRicCov(int[] x){
-        stampaArrayRicCov(x, x.length-1);
+    static void stampaArrayRicCov(int[] x) {
+        stampaArrayRicCov(x, x.length - 1);
     }
 
-    static void stampaArrayRicCov(int[] x, int i){
-        if(i >= 0){
-            System.out.println(x[x.length-1-i] + "");
-            stampaArrayRicCov(x, i-1);
-        }else{
+    static void stampaArrayRicCov(int[] x, int i) {
+        if (i >= 0) {
+            System.out.println(x[x.length - 1 - i] + "");
+            stampaArrayRicCov(x, i - 1);
+        } else {
             System.out.println();
         }
     }
 
-    static void stampaArrayRicDicotomic(int x[]){
-        stampaArrayRicDicotomic(x, 0, x.length-1);
+    static void stampaArrayRicDicotomic(int x[]) {
+        stampaArrayRicDicotomic(x, 0, x.length - 1);
     }
 
-    static void stampaArrayRicDicotomic(int x[], int left, int right){
-        int mid = (left+right)/2;
+    static void stampaArrayRicDicotomic(int x[], int left, int right) {
+        int mid = (left + right) / 2;
 
-        if(left != right){
+        if (left != right) {
             stampaArrayRicDicotomic(x, left, mid);
-            stampaArrayRicDicotomic(x, mid+1, right);
-        }else{
+            stampaArrayRicDicotomic(x, mid + 1, right);
+        } else {
             System.out.println(x[left]);
         }
     }
 
-    
 }
---
+++ Lazynadbitwise.java
@@ -0,0 +1,14 @@
+class Lazyandbitwise{
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 100;
+
+        //NOTE - LAZY
+        System.out.println((a!=0) && (b/a > 100)); // il secondo statement viene considerato sono se il primo è true o non da errore
+        System.out.println((a!=0) || (b/a > 100)); // il secondo statement viene considerato solo se il primo non da errore 
+        
+        //NOTE - BITWISE
+        System.out.println((a!=0) & (b/a > 100)); // vengono considerati entrambi gli statement (rispetta la tavola di verità &). se ci sono varaibili intere e non booleane esegue l'operazione & tra bit
+        System.out.println((a!=0) | (b/a > 100)); // vengono considerati entrambi gli statement (rispetta la tavola di verità |). se ci sono varaibili intere e non booleane esegue l'operazione | tra bit
+    }
+}
